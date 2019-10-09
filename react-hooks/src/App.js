1ec8898e4f07c2cf347c7c52844868aea0a5bc6a
starrpatty28 <starrpatty28@gmail.com>
2019-10-09 16:09:17 -0700

    Created a directory called tables and added code for the tables abd buttons. Add skeleton for tables

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import UserTable from './tables/UserTable'
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable />
         </div>
       </div>
     </div>
