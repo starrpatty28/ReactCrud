1c59a5f2fb6fe0dbca1aa336eb8ba134a5d117ce
starrpatty28 <starrpatty28@gmail.com>
2019-10-09 16:30:28 -0700

    Brought in some dummy data and the useState import from React

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,6 +3,15 @@ import UserTable from './tables/UserTable'
 
 
 const App = () => {
+
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(userData)
+
   return (
     <div className="container">
       <h1>React CRUD App with Hooks</h1>
